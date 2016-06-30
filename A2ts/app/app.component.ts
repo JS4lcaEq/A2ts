73a5dbec196b583244ae28705ce5914feeda2000
JS4lcaEq <condorrr@gmail.com>
2016-06-30 22:37:15 +0300

    rytrty

---
+++ A2ts/app/app.component.ts
@@ -0,0 +1,9 @@
+﻿import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: '<h1>My 1 First Angular 2 App [{{test}}]</h1><p>p</p>[<ul><li *ngFor="let item of arr" >{{item}}</li></ul>]'
+})
+export class AppComponent {
+    test = "test";
+    arr = [1,2,3,4,5]; 
+}
